5cf17245db71a96045c8f851002c7de4a6797442
sumaikakaira <122632122+sumaikakaira@users.noreply.github.com>
2023-04-11 04:02:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// assignment no 4
+document.write('<h1> Registration Form</h1>');
+document.write("<b>Your Name</b>");
+document.write('</br>');
+document.write('</br>');
+document.write("<b>LastName</b>");
+document.write('</br>');
+document.write('</br>');
+document.write("<b>Enter your Email</b>")
+document.write('</br>');
+document.write("</br>");
+document.write("<b>Enter your number</b>")
+document.write('</br>');
+document.write("</br>");
+document.write('<b>Date of birth</b>')
+document.write('</br>');
+document.write("</br>");
+document.write('<b>Age</b>')
+document.write('</br>');
+document.write("</br>");
+document.write('<b>city</b>')
+document.write('</br>');
+document.write("</br>");
+document.write('<b>Country</b>')
+document.write('</br>');
+document.write("</br>");
+document.write('Religion')
+document.write('</br>');
+document.write("</br>");
+document.write("passward")
+document.write('</br>');
+document.write("</br>");
+// login detail
+var UserName=prompt('what is your Name');
+if(UserName==="Asadkakar"){
+    document.write('you are enrolled')
+}
+else{
+    document.write('sorry!you are not enrolled')
+};
